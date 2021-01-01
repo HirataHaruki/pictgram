c5a09e25b16e02824e53281929ce3b0d6cb30bb7
平田遥暉 <haru922611@gmail.com>
2021-01-01 03:46:14 +0000

    Rails 04 課題　再提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  VALIDATES_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALIDATES_PASS = /A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, length: { in: 8..32 }, format: { with: /A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :email, presence: true, format: { with: VALIDATES_EMAIL }
+  validates :password, length: { in: 8..32 }, format: { with: VALIDATES_PASS }
   
   has_secure_password
+  
+  has_many :topics
 end
