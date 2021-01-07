6111f086e95a28162cec31034f3c0bf01df79d41
平田遥暉 <haru922611@gmail.com>
2021-01-07 15:40:02 +0000

    Rails07 課題

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
+  get '/comments/:topic_id/new', to: 'comments#new', as: 'comments'
+  post '/comments/:topic_id/create', to: 'comments#create', as: 'comments_create'
+  
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
